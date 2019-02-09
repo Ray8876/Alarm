5f21d5656ef1b00c6a4dccb151ae06126b28be05
Ray8876 <1528609904@qq.com>
2019-02-09 14:23:36 +0800

    v4.0

---
+++ Alarm/Form1.Designer.cs
@@ -0,0 +1,328 @@
+﻿namespace Alarm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.htmlToolTip1 = new MetroFramework.Drawing.Html.HtmlToolTip();
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.Label1 = new MetroFramework.Controls.MetroLabel();
+            this.Label3 = new MetroFramework.Controls.MetroLabel();
+            this.Label2 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox3 = new MetroFramework.Controls.MetroTextBox();
+            this.button2 = new MetroFramework.Controls.MetroButton();
+            this.button3 = new MetroFramework.Controls.MetroButton();
+            this.button4 = new MetroFramework.Controls.MetroButton();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.metroButton3 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("黑体", 18F);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(27, 123);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(241, 196);
+            this.listBox1.TabIndex = 9;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "闹钟";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // htmlToolTip1
+            // 
+            this.htmlToolTip1.OwnerDraw = true;
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton1.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.metroButton1.Location = new System.Drawing.Point(288, 80);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(75, 25);
+            this.metroButton1.TabIndex = 16;
+            this.metroButton1.Text = "添加";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Label1.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.Label1.Location = new System.Drawing.Point(27, 38);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(84, 25);
+            this.Label1.TabIndex = 17;
+            this.Label1.Text = "当前时间";
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Label3.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.Label3.Location = new System.Drawing.Point(147, 38);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(0, 0);
+            this.Label3.TabIndex = 18;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.Label2.FontWeight = MetroFramework.MetroLabelWeight.Regular;
+            this.Label2.Location = new System.Drawing.Point(27, 80);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(84, 25);
+            this.Label2.TabIndex = 19;
+            this.Label2.Text = "添加闹钟";
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(11, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox1.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.metroTextBox1.Lines = new string[] {
+        "00"};
+            this.metroTextBox1.Location = new System.Drawing.Point(147, 80);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(33, 23);
+            this.metroTextBox1.TabIndex = 20;
+            this.metroTextBox1.Text = "00";
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox2
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox2.CustomButton.Image = null;
+            this.metroTextBox2.CustomButton.Location = new System.Drawing.Point(11, 1);
+            this.metroTextBox2.CustomButton.Name = "";
+            this.metroTextBox2.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox2.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox2.CustomButton.TabIndex = 1;
+            this.metroTextBox2.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox2.CustomButton.UseSelectable = true;
+            this.metroTextBox2.CustomButton.Visible = false;
+            this.metroTextBox2.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox2.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.metroTextBox2.Lines = new string[] {
+        "00"};
+            this.metroTextBox2.Location = new System.Drawing.Point(191, 80);
+            this.metroTextBox2.MaxLength = 32767;
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.PasswordChar = '\0';
+            this.metroTextBox2.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox2.SelectedText = "";
+            this.metroTextBox2.SelectionLength = 0;
+            this.metroTextBox2.SelectionStart = 0;
+            this.metroTextBox2.ShortcutsEnabled = true;
+            this.metroTextBox2.Size = new System.Drawing.Size(33, 23);
+            this.metroTextBox2.TabIndex = 21;
+            this.metroTextBox2.Text = "00";
+            this.metroTextBox2.UseSelectable = true;
+            this.metroTextBox2.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox2.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // metroTextBox3
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox3.CustomButton.Image = null;
+            this.metroTextBox3.CustomButton.Location = new System.Drawing.Point(11, 1);
+            this.metroTextBox3.CustomButton.Name = "";
+            this.metroTextBox3.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox3.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox3.CustomButton.TabIndex = 1;
+            this.metroTextBox3.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox3.CustomButton.UseSelectable = true;
+            this.metroTextBox3.CustomButton.Visible = false;
+            this.metroTextBox3.FontSize = MetroFramework.MetroTextBoxSize.Tall;
+            this.metroTextBox3.FontWeight = MetroFramework.MetroTextBoxWeight.Bold;
+            this.metroTextBox3.Lines = new string[] {
+        "00"};
+            this.metroTextBox3.Location = new System.Drawing.Point(235, 80);
+            this.metroTextBox3.MaxLength = 32767;
+            this.metroTextBox3.Name = "metroTextBox3";
+            this.metroTextBox3.PasswordChar = '\0';
+            this.metroTextBox3.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox3.SelectedText = "";
+            this.metroTextBox3.SelectionLength = 0;
+            this.metroTextBox3.SelectionStart = 0;
+            this.metroTextBox3.ShortcutsEnabled = true;
+            this.metroTextBox3.Size = new System.Drawing.Size(33, 23);
+            this.metroTextBox3.TabIndex = 22;
+            this.metroTextBox3.Text = "00";
+            this.metroTextBox3.UseSelectable = true;
+            this.metroTextBox3.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox3.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // button2
+            // 
+            this.button2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.button2.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.button2.Location = new System.Drawing.Point(288, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "删除";
+            this.button2.UseSelectable = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.button3.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.button3.Location = new System.Drawing.Point(288, 231);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 57);
+            this.button3.TabIndex = 24;
+            this.button3.Text = "最小化\r\n到托盘";
+            this.button3.UseSelectable = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // button4
+            // 
+            this.button4.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.button4.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.button4.Location = new System.Drawing.Point(288, 294);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 25);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "关于";
+            this.button4.UseSelectable = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton2.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.metroButton2.Location = new System.Drawing.Point(288, 38);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(75, 25);
+            this.metroButton2.TabIndex = 26;
+            this.metroButton2.Text = "停止闹铃";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // metroButton3
+            // 
+            this.metroButton3.FontSize = MetroFramework.MetroButtonSize.Medium;
+            this.metroButton3.FontWeight = MetroFramework.MetroButtonWeight.Regular;
+            this.metroButton3.Location = new System.Drawing.Point(288, 200);
+            this.metroButton3.Name = "metroButton3";
+            this.metroButton3.Size = new System.Drawing.Size(75, 25);
+            this.metroButton3.TabIndex = 27;
+            this.metroButton3.Text = "修改闹铃";
+            this.metroButton3.UseSelectable = true;
+            this.metroButton3.Click += new System.EventHandler(this.metroButton3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 353);
+            this.Controls.Add(this.metroButton3);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.metroTextBox3);
+            this.Controls.Add(this.metroTextBox2);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.metroButton1);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Style = MetroFramework.MetroColorStyle.Default;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private MetroFramework.Drawing.Html.HtmlToolTip htmlToolTip1;
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroLabel Label1;
+        private MetroFramework.Controls.MetroLabel Label3;
+        private MetroFramework.Controls.MetroLabel Label2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox3;
+        private MetroFramework.Controls.MetroButton button2;
+        private MetroFramework.Controls.MetroButton button3;
+        private MetroFramework.Controls.MetroButton button4;
+        private MetroFramework.Controls.MetroButton metroButton2;
+        private MetroFramework.Controls.MetroButton metroButton3;
+    }
+}
+
---
+++ Alarm/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using MetroFramework.Forms;
+using System.Xml;
+
+namespace Alarm
+{
+    public partial class Form1 : MetroForm
+    {
+        SoundPlayer player = new SoundPlayer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Interval = (1000) * (1);
+            timer1.Enabled = true;
+            timer1.Start();
+
+            try
+            {
+                //读取配置
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("sound.xml");
+                XmlNodeList xml = xmlDoc.SelectSingleNode(@"configuration/path").ChildNodes;
+                foreach (XmlNode Node in xml)
+                {
+                    player.SoundLocation = Node.InnerText;
+                }
+                
+            }
+            catch (Exception)
+            {
+
+            }
+            
+            notifyIcon1.Visible = false;
+
+            try
+            {
+                //读取配置
+                XmlDocument xmlDoc = new XmlDocument();
+                xmlDoc.Load("data.xml");
+                XmlNodeList xml = xmlDoc.SelectNodes(@"configuration/time_data");
+                foreach (XmlNode Node in xml)
+                {
+                    listBox1.Items.Add(Node.InnerText);
+                }
+            }
+            catch(Exception)
+            {
+
+            }
+
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Label3.Text = DateTime.Now.Hour.ToString("D2") + ":" + DateTime.Now.Minute.ToString("D2") + ":" + DateTime.Now.Second.ToString("D2");
+
+            for(int i = 0; i < listBox1.Items.Count; i++)
+            {
+                if (listBox1.Items[i].ToString() == Label3.Text.ToString())
+                {
+                    player.Load();
+                    player.PlayLooping();
+                }
+
+            }
+
+        }
+
+
+        private void domainUpDown1_SelectedItemChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            this.Hide();
+            this.ShowInTaskbar = false;
+            this.notifyIcon1.Visible = true;
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon1.Visible = false;
+            this.ShowInTaskbar = true;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            string add_time = "";
+            string temp = metroTextBox1.Text;
+            if (!panduan(temp, 1))
+            {
+                MessageBox.Show("输入有误");
+                return;
+            }
+            add_time = add_time + string.Format("{0:D2}", temp);
+            temp = metroTextBox2.Text;
+            if (!panduan(temp, 2))
+            {
+                MessageBox.Show("输入有误");
+                return;
+            }
+            add_time = add_time + ":" + string.Format("{0:D2}", temp);
+            temp = metroTextBox3.Text;
+            if (!panduan(temp, 3))
+            {
+                MessageBox.Show("输入有误");
+                return;
+            }
+            add_time = add_time + ":" + string.Format("{0:D2}", temp);
+            listBox1.Items.Add(add_time);
+
+            update_xml();
+        }
+
+        private void update_xml()
+        {
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlDeclaration xmlDeclar;
+            xmlDeclar = xmlDocument.CreateXmlDeclaration("1.0", "utf-8", null);
+            xmlDocument.AppendChild(xmlDeclar);
+
+            XmlElement xmlElement = xmlDocument.CreateElement("", "configuration", "");
+            xmlDocument.AppendChild(xmlElement);
+            XmlNode root = xmlDocument.SelectSingleNode("configuration");
+
+
+            for (int i = 0; i < listBox1.Items.Count; i++)
+            {
+                XmlElement temp = xmlDocument.CreateElement("time_data");
+                temp.InnerText = listBox1.Items[i].ToString();
+                root.AppendChild(temp);
+            }
+            xmlDocument.Save("data.xml");
+        }
+
+        private bool panduan(string temp, int v)
+        {
+            if (temp == "" || temp.Length != 2)
+                return false;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                if (!Char.IsNumber(temp, i))
+                    return false;
+            }
+            if (v == 1)
+            {
+                if (Convert.ToInt32(temp)>=24 || Convert.ToInt32(temp) < 0)
+                {
+                    return false;
+                }
+            }
+            else if (v == 2)
+            {
+                if (Convert.ToInt32(temp) >= 60 || Convert.ToInt32(temp) < 0)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                if (Convert.ToInt32(temp) >= 60 || Convert.ToInt32(temp) < 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex != -1)
+                listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            update_xml();
+        }
+
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("Design By Ray8876!\nver 4.0");
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void metroButton3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show(dlg.FileName);
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlDeclaration xmlDeclar;
+                xmlDeclar = xmlDocument.CreateXmlDeclaration("1.0", "utf-8", null);
+                xmlDocument.AppendChild(xmlDeclar);
+
+                XmlElement xmlElement = xmlDocument.CreateElement("", "configuration", "");
+                xmlDocument.AppendChild(xmlElement);
+                XmlNode root = xmlDocument.SelectSingleNode("configuration");
+
+
+                XmlElement temp = xmlDocument.CreateElement("path");
+                temp.InnerText = dlg.FileName;
+                root.AppendChild(temp);
+
+                xmlDocument.Save("sound.xml");
+                player.SoundLocation = dlg.FileName;
+            }
+
+        }
+    }
+}
